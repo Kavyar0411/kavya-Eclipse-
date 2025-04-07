f675d1efcb5cbbab48f9ae0a5672cbed7a4e288a
Kavyar0411 <152874386+Kavyar0411@users.noreply.github.com>
2025-04-07 11:56:04 +0530

    Add files via upload

---
+++ myproject/Encapsulation/MainCode.java
@@ -0,0 +1,15 @@
+package com.mvj.Encapsulation;
+
+public class MainCode {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encap E1 = new Encap();
+		E1.setSerialNum(101);
+		E1.setName("mona");
+		E1.setAge(15);
+		System.out.println(E1);
+		
+
+	}
+
+}
