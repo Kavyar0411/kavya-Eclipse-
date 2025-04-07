99dfa942ef2a07dbdcc3c42882787b446bedc70c
Kavyar0411 <152874386+Kavyar0411@users.noreply.github.com>
2025-04-07 11:41:25 +0530

    Add files via upload

---
+++ myproject/Ticketbooking/MainTicket.java
@@ -0,0 +1,15 @@
+package com.mvj.ticketbooking;
+
+public class MainTicket {
+	public static void main(String[] args) {
+		
+		Tickets t1=new Tickets();
+		t1.available_tickets();
+		t1.available_tickets();
+		t1.available_tickets();
+		t1.available_tickets();
+		t1.available_tickets();
+		
+	}
+
+}
