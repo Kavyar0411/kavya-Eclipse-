690a7e41d6fc3e3642edbb2f7f93152ec2a6fecf
Kavyar0411 <152874386+Kavyar0411@users.noreply.github.com>
2025-04-17 18:06:52 +0530

    Add files via upload

---
+++ myproject/Comparator/Demo.java
@@ -0,0 +1,26 @@
+package com.mvj.collection_comparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Student> ar=new ArrayList<Student>();
+		ar.add(new Student(12,"abc","bang"));
+		ar.add(new Student(6,"tye","mysore"));
+		ar.add(new Student(1,"mno","ranchi"));
+		System.out.println("--------------");
+		Collections.sort(ar,new SortRoll());
+		for( int i=0; i<ar.size(); i++) {
+			System.out.println(ar.get(i));
+		}
+		Collections.sort(ar,new SortName());
+		for( int i=0; i<ar.size(); i++) {
+			System.out.println(ar.get(i));
+		}
+
+	}
+
+}
