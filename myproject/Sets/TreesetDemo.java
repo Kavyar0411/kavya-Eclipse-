e6239972aa32c4dfc5ea3182f1b613ec983126ce
Kavyar0411 <152874386+Kavyar0411@users.noreply.github.com>
2025-04-22 13:33:37 +0530

    Add files via upload

---
+++ myproject/Sets/TreesetDemo.java
@@ -0,0 +1,19 @@
+package com.mvj.Collection;
+
+import java.util.TreeSet;
+
+public class TreesetDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet <String> t=new TreeSet<String>();
+		t.add("HTML");
+		t.add("Java");
+		t.add("HTML");
+		//t.add(null);
+		System.out.println("course are"+t);
+				
+
+	}
+
+}
